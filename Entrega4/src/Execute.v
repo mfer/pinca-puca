cf6c27fe23c195ec2efd8f9ee363b3cc5e55e47a
mfer <sudolshw@gmail.com>
2014-05-01 23:51:49 -0300

    execute almost there

--- Entrega4/src/Execute.v
+++ Entrega4/src/Execute.v
@@ -5,6 +5,8 @@
 *   do estágio de decodificação os operandos e a operação 
 *   a ser realizada. O resultado é passado para o estágio de memória
 *   Depende da Alu e do Shifter.
+*       Tenho feito o include na compilação, nesse caso:
+*         iverilog src/Execute.v -o bin/Execute ../Entrega1/src/Alu.v ../Entrega1/src/Shifter.v
 */
 
 module Execute (    clock,     reset,   id_ex_selalushift,  id_ex_selimregb, 
@@ -47,7 +49,6 @@ module Execute (    clock,     reset,   id_ex_selalushift,  id_ex_selimregb,
     //sinais a serem repassados para o estágio de memória
     input               id_ex_selwsource;
     input       [4:0]   id_ex_regdest;
-
     //indica escrita no banco de registradores
     input               id_ex_writereg;
 
@@ -56,23 +57,19 @@ module Execute (    clock,     reset,   id_ex_selalushift,  id_ex_selimregb,
     input               id_ex_writeov;
 
     /****Fetch****/
-
     //indica que um stall deve ser inserido, setar em  1..
     //..se id_ex_readmem==1 ou id_ex_writemem==1
     output reg          id_if_stall;
 
     /****Memory****/
-
     //recebem o id_ex_<correspondente>
     output reg          id_mem_readmem;
     output reg          id_mem_writemem;
     output reg  [31:0]  id_mem_regb;
     output reg          id_mem_selwsource;
     output reg  [4:0]   id_mem_regdest;
-
     //recebe (!aluov | id_ex_writeov) & id_ex_writereg
     output reg          id_mem_writereg;
-
     //recebe a saida do shifter caso id_ex_selalushift==1
     //senão recebe a saída da Alu
     output reg  [31:0]  id_mem_wbvalue;
@@ -83,15 +80,16 @@ module Execute (    clock,     reset,   id_ex_selalushift,  id_ex_selimregb,
     output  [31:0]      aluout;
     //input   [2:0]       op;
     //input           unsig;
-    output          compout;
+    output          compout; //esse sinal é irrelevante nesse trabalho
     //output          overflow;
 
     /****Shifter****/
-    input [31:0] in,
-    input [1:0] shiftop,
-    input [4:0] shiftamt,
-    output [31:0] result
+    //input [31:0] in;
+    //input [1:0] shiftop;
+    //input [4:0] shiftamt;
+    output [31:0] result;
 
+    assign id_mem_writereg = ((!overflow | id_ex_writeov) & id_ex_writereg);
 
     always @(posedge clock or negedge reset )begin
         if (~reset) begin
@@ -120,14 +118,23 @@ module Execute (    clock,     reset,   id_ex_selalushift,  id_ex_selimregb,
             end
         end
 
-        if (id_ex_selalushift) begin
+        if (~id_ex_selalushift) begin
             //resultado da Alu 
+            id_mem_wbvalue <= aluout;
         end
         else begin
             //resultado do Shifter
+            id_mem_wbvalue <= result;
         end
+
+        //repassadno valores para o estágio de memória        
+        id_mem_selwsource <= id_ex_selwsource;
+        id_mem_regdest <= id_ex_regdest;
+        id_mem_readmem <= id_ex_readmem;
+        id_mem_writemem <= id_ex_writemem;
+        id_mem_regb <= id_ex_regb;
     end
 
     Alu ALU (.a(id_ex_rega), .b(b), .aluout(aluout), .op(id_ex_aluop), .unsig(id_ex_unsig), .compout(compout), .overflow(overflow));
-    Shifter SHIFTER(.in(id_ex_regb), .shiftop.(id_ex_shiftop), .shiftamt(id_ex_shiftamt), .result(result));
+    Shifter SHIFTER(.in(id_ex_regb), .shiftop(id_ex_shiftop), .shiftamt(id_ex_shiftamt), .result(result));
 endmodule
