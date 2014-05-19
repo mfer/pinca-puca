b62340aeefbb00fea77346c6561d954c61a45528
Manassés Ferreira Neto <sudolshw@gmail.com>
2014-05-19 18:14:40 -0300

    Update Top.v

--- Entrevista1/Top.v
+++ Entrevista1/Top.v
@@ -1,22 +1,26 @@
 module Top(SW,KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7);
 
 	input	[16:0]	SW; // 7:0 - A; 15:8- B; 16- unsig
-	output	[7:0]	HEX0;
-	output	[7:0]	HEX1;
- 	output	[7:0]	HEX2;
-	output	[7:0]	HEX3;	
-	output	[7:0]	HEX4;
-	output	[7:0]	HEX5;
-	output 	[7:0] 	HEX6;
-	output	[7:0] 	HEX7;
+	output [7:0]	HEX0;
+	output [7:0]	HEX1;
+ 	output [7:0]	HEX2;
+	output [7:0]	HEX3;	
+	output [7:0]	HEX4;
+	output [7:0]	HEX5;
+	output [7:0] 	HEX6;
+	output [7:0] 	HEX7;
 	output	[0:0]	LEDR;
 	input	[3:1]	KEY;
 
-	parameter extensor = 23'b000000000000000000000000;
-	wire	[31:0]	aluout;	
+	parameter extensor_positive = 24'b000000000000000000000000;
+	
+	wire	[31:0]	aluout;
 	wire compout;		
-
-	Alu  A(.a({SW[7:0], extensor}), .b ({SW[15:8], extensor}), .aluout(aluout), .unsig (SW[16]), .op (KEY), .compout(compout),.overflow (LEDR) );
+	
+	Alu  A(.a({SW[7:0], extensor}), 
+	       .b({SW[15:8], extensor}), 
+			 .aluout(aluout), .unsig (SW[16]), 
+			 .op (KEY), .compout(compout),.overflow (LEDR) );
 	
 	OutConverter H0 (.aluout(aluout[3:0]),    .dhex(HEX0));
 	OutConverter H1 (.aluout(aluout[7:4]),    .dhex(HEX1));
@@ -26,5 +30,4 @@ module Top(SW,KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7);
 	OutConverter H5 (.aluout(aluout[23:20]), .dhex(HEX5)); 
 	OutConverter H6 (.aluout(aluout[27:24]), .dhex(HEX6));
 	OutConverter H7 (.aluout(aluout[31:28]), .dhex(HEX7));
-
 endmodule
